2ab2352595564e2af7553b8c59371a2c90146ac1
Amir <amirsahrani1@gmail.com>
2022-10-31 18:24:41 +0100

    fix

--- counter.py
+++ counter.py
@@ -6,7 +6,7 @@ now = datetime.now()
 f.write(f"Date: {now}\n")
 inputheads = input("Select the key you want to press for heads: ")
 inputtails = input("Select the key you want to press for tails: ")
-f.write(f"keys for heads is :{inputheads}, key for tails: {inputtails}")
+f.write(f"keys for heads is :{inputheads}, key for tails: {inputtails}\n")
 
 counter = 0
 string = ""
