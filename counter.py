9b2ac1b9242c144a42660aac2ee4be0c9611daf5
Amir <amirsahrani1@gmail.com>
2022-11-16 09:53:22 +0100

    made format csv

--- counter.py
+++ counter.py
@@ -1,30 +1,56 @@
+# pip install playsound==1.2.2      You need this specific version of the package for it to work
 from random import choice
 from datetime import datetime
 from playsound import playsound
 
-f = open("HeadsandTails.txt", "a")
-now = datetime.now()
-f.write(f"Date: {now}\n")
-inputheads = input("Select the key you want to press for heads: ")
-inputtails = input("Select the key you want to press for tails: ")
-f.write(f"keys for heads is :{inputheads}, key for tails: {inputtails}\n")
+
 
 counter = 0
 string = ""
+
+f = open("HeadsandTails.txt", "a")
+# f.write(f"Date: {now}\n")
+coin = input("Which coin are you using?")
+inputheads  = input("Select the key you want to press for heads: ")
+inputtails  = input("Select the key you want to press for tails: ")
+failedheads = input("Select the key you want to press for a failed heads: ")
+failedtails = input("Select the key you want to press for a failed tails: ")
+undo        = input("Select the key you want to use for undoing you previous input: ")
+change_coin = input("Select the key you want to use to change the coin: ")
+
+dict = {inputheads: "h", inputtails: "t", failedheads : "x", failedtails: "y"}
+
 while True:
     inputs = []
     start = choice(["Heads","Tails"])
     print(f"!!!!!!!!!!!!!!!!!!!! Please start with {start} facing up !!!!!!!!!!!!!!!!")
-    playsound('C:\\Users\\amisa\\Documents\\Python\\Thesis\\bell.wav')
+    playsound("bell.wav")
     while True:
         while True:
             x = input(f"Last outcome for heads press: \"{inputheads}\" for tails press \"{inputtails}\": ")
+            if x == change_coin:
+                coin = input("Which coin are you using?")
+                break
             if x in [inputheads,inputtails]:
-                inputs.append(x)
+                inputs.append(dict[x])
                 counter += 1
                 print(string.join(inputs))
-                break
-        if counter % 100 == 0:
+            elif x in [failedheads, failedtails]:
+                inputs.append(dict[x])
+                print(string.join(inputs))
+                continue
+            elif x == undo:
+                inputs.pop()
+                counter = counter -1
+                print(string.join(inputs))
+                continue
             break
-    f.write(f"{string.join(inputs)}\n")
-    
+        if counter % 5 == 0:
+            break
+    if input != []:
+        now = datetime.now()
+        now = now.strftime("%Y-%m-%d %H:%M:%S")
+        f.write(f"{now},{coin},{start},{string.join(inputs)}\n")
+
+## You can always press ctrl + c in the terminal to terminate the process, doing so in the middle of a block will discard that specific block, 
+# if you want to save it you can manually copy it from the terminal and paste it in the .txt file
